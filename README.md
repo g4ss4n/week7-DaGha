557e7930d48a9eee478b32f65d8feb75de2365e6
Ghassan <g4ss4n@gmail.com>
2019-08-20 15:14:34 +0300

    database created

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# week7-DaGha
+# DaGha APP
+
+This app is about personality tests, means
+We can guess your character according to your choices in our test!
+
+---
+
+### How to use
+
+First you have to register - by clicking on REGISTER and filling the forum :)
+
+Then you will have to answer some questions and and you will see the table of each users with his/her results!
+
+---
+
+### Workflow
+
+1.  First we came up with the idea of what the website should be about.
+2.  We designed the the front-end using html, css and javascript!
+3.  We created the server and the router (also the handler)
+4.  we created the database (check the diagram below) and hosted it on Heruko.
+
+---
+
+### Database Diagram
+
+We have two tables here
+
+- users
+- results
+
+![](https://i.imgur.com/5Z5bFqs.png)
---
+++ src/database/db_build.sql
@@ -0,0 +1,30 @@
+BEGIN;
+
+    DROP TABLE IF EXISTS users CASCADE;
+    DROP TABLE IF EXISTS results CASCADE;
+
+    CREATE TABLE users(
+        user_id SERIAL PRIMARY KEY,
+        username VARCHAR(15) NOT NULL,
+        first_name VARCHAR(30) NOT NULL,
+        last_name VARCHAR (30) NOT NULL);
+
+    CREATE TABLE results(
+        user_id INTEGER NOT NULL,
+        q1 INTEGER NOT NULL,
+        q2 INTEGER NOT NULL,
+        q3 INTEGER NOT NULL,
+        q4 INTEGER NOT NULL,
+        result VARCHAR(100));
+
+    INSERT INTO users (username,first_name,last_name)
+    VALUES
+    ('g4ss4n','Ghassan', 'Gharzuzy'),
+    ('danafalah', 'Dana', 'Falah');
+
+    INSERT INTO results (user_id,q1,q2,q3,q4,result)
+    VALUES
+        (1, 3, 2, 3, 1, 'Great'),
+        (2, 3, 2, 4, 1, 'Not good');
+
+    COMMIT;
