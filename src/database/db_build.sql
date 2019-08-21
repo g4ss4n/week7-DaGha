716ca21956c7a7bdb1686eeaa3afe35c8b877bce
Ghassan <g4ss4n@gmail.com>
2019-08-21 14:23:31 +0300

    getData function

--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -6,6 +6,7 @@ BEGIN;
     CREATE TABLE users(
         user_id SERIAL PRIMARY KEY,
         username VARCHAR(15) NOT NULL,
+        password VARCHAR NOT NULL,
         first_name VARCHAR(30) NOT NULL,
         last_name VARCHAR (30) NOT NULL);
 
@@ -17,14 +18,14 @@ BEGIN;
         q4 INTEGER NOT NULL,
         result VARCHAR(100));
 
-    INSERT INTO users (username,first_name,last_name)
+    INSERT INTO users (username,password,first_name,last_name)
     VALUES
-    ('g4ss4n','Ghassan', 'Gharzuzy'),
-    ('danafalah', 'Dana', 'Falah');
+    ('g4ss4n','123456', 'Ghassan', 'Gharzuzy'),
+    ('danafalah','123456', 'Dana', 'Falah');
 
     INSERT INTO results (user_id,q1,q2,q3,q4,result)
     VALUES
-        (1, 3, 2, 3, 1, 'Great'),
-        (2, 3, 2, 4, 1, 'Not good');
+        (1, 3, 2, 3, 1, 'Handsome, lovely, handsome again ofc'),
+        (2, 3, 2, 4, 1, 'ugly, so damn UGLY');
 
     COMMIT;
